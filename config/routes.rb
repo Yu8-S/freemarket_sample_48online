e6188144940d09660f15e7a2a196e48f74b49488
Yuya <yu8.0101@icloud.com>
2019-05-23 16:36:42 +0900

    All commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :profiles
   resources :profile
   resources :sign_out
+  resources :cards
   resources :identifications
   resources :users, only: [:index]
 end
