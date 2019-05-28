41c5726a40969e0824ef03959cd8dba782db5f7b
Yuya <yu8.0101@icloud.com>
2019-05-28 17:23:32 +0900

    All commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      AWS_ACCESS_KEY_ID: ENV["S3_ACCESS_KEY"],
+      AWS_SECRET_ACCESS_KEY: ENV["S3_SECRET_KEY"],
       region: 'ap-northeast-1'
   }
 
