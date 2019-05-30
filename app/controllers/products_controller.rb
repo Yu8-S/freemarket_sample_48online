989537d68184c4c7ed0c1fdf515ecf22e161516a
Yuya <yu8.0101@icloud.com>
2019-05-30 19:10:40 +0900

    Update products_controller.rb
    各カテゴリーごとにDBより取得。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    @womens = Product.where("category = ?","レディース").limit(4).order(:created_at)
+    @mens = Product.where("category = ?","メンズ").limit(4).order(:created_at)
+    @kids = Product.where("category = ?","ベビー・キッズ").limit(4).order(:created_at)
+    @cosmetics = Product.where("category = ?","コスメ・香水・美容").limit(4).order(:created_at)
+    @chanel = Product.where("brand = ?","シャネル").limit(4).order(:created_at)
+    @vuitton = Product.where("brand = ?","ルイヴィトン").limit(4).order(:created_at)
+    @supreme = Product.where("brand = ?","シェプリーム").limit(4).order(:created_at)
+    @nike = Product.where("brand = ?","ナイキ").limit(4).order(:created_at)
   end
 
   def show
