5f907f6f49f2b1e41f260a027aeac6ce5fef4117
Yuya <yu8.0101@icloud.com>
2019-06-04 21:50:35 +0900

    Update evaluations_controller.rb
    エラーハンドリングを追記

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,6 +1,12 @@
 class EvaluationsController < ApplicationController
   def create
-    Evaluation.create(comment_params)
+    @evaluation = Evaluation.create(comment_params)
+    if @evaluation.save
+      redirect_to product_path(comment_params[:product_id])
+    else
+      flash[:danger] = "コメント送信を失敗しました"
+      render :create
+    end
   end
 
   def new
